c25242291e31e1c78700026b44b53020fa0a905a
Nguyên Devs <144598460+NguyenDevs@users.noreply.github.com>
2026-05-10 07:28:15 +0700

    Update threejs-bg.js

--- assets/js/threejs-bg.js
+++ assets/js/threejs-bg.js
@@ -650,8 +650,8 @@
       if (musicEnable) {
         coreLight.intensity = (0.8 + audioIntensity * 35) * coreIntro;
         glowOrb.scale.setScalar((6.0 + audioIntensity * 2) * (0.2 + 0.8 * coreIntro));
-        blackHole.scale.setScalar(Math.max(0.5, 1.0 - audioIntensity * 0.8));
-        bhGlow.scale.setScalar((1.3 + audioIntensity * 2.5) * coreIntro);
+        blackHole.scale.setScalar(Math.max(0.35, 1.0 - audioIntensity * 0.8));
+        bhGlow.scale.setScalar((1.3 + audioIntensity * 1.5) * coreIntro);
         bhGlow.material.opacity = 0.6 + audioIntensity * 1.2;
       } else {
         coreLight.intensity = (4 + Math.sin(t * 2) * 2) * coreIntro;
